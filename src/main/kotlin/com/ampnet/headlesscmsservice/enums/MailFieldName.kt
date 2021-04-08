53dd87e3ac5c710bf8ed40b6deb3c10363bb1827
Josip Koncurat <jkoncura@gmail.com>
2021-04-08 11:03:42 +0200

    breaking complex mails with conditionals (#2)
    * breaking complex mails with conditionals
    
    * missing mail
    
    * add required field
    
    Co-authored-by: Josip KonÄurat <josipkoncurat@Josips-MacBook-Pro.local>

--- src/main/kotlin/com/ampnet/headlesscmsservice/enums/MailFieldName.kt
+++ src/main/kotlin/com/ampnet/headlesscmsservice/enums/MailFieldName.kt
@@ -9,5 +9,7 @@ enum class MailFieldName(val value: String) {
     AMOUNT("{{amount}}"),
     FAILED_RECIPIENTS("{{failedRecipients}}"),
     FIRST_NAME("{{firstName}}"),
-    PROJECT_DESCRIPTION("{{projectDescription}}");
+    PROJECT_DESCRIPTION("{{projectDescription}}"),
+    COOP("{{coop}}"),
+    LAST_NAME("{{lastName}}");
 }
